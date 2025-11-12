5137d193f236e704b85ffb0bb5b38f68edeaaf46
Jchosenn <jonathanchosen79@gmail.com>
2025-11-12 15:48:47 +0100

    chore: Project setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='flex flex-col justify-center items-center  min-h-screen'>
+      <h2 className='text-2xl font-giest font-extrabold'>Welcome</h2>
+      <h2 className='text-2xl font-geist font-extrabold'>hel</h2>
+      <h2 className='text-2xl font-extrabold'>Welcome</h2>
+
+    </div>
+    
+      
+  )
+}
+
+export default App
