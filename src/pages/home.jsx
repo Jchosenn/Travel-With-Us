17b0a5f961d11f6f41ed487b2135ae2186118ab1
Jchosenn <jonathanchosen79@gmail.com>
2025-11-15 13:53:59 +0100

    feat: added feat section

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Header } from './header'
 import { Destination } from './destination'
+import { About } from './about'
 
 export const Home = () => {
   return (
     <div className=' dark:bg-black/95 dark:text-white'>
       <Header/>
       <Destination/>
+      <About/>
       </div>
   )
 }
--- src/router.jsx
+++ src/router.jsx
@@ -1,19 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Home } from "./pages/home";
-import { Header } from "./pages/header";
-import { Destination } from "./pages/destination";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
-  },
-  {
-    path: "/header",
-    element: <Header/>
-  },
-  {
-    path: "/destination",
-    element: <Destination/>
   }
 ]);
